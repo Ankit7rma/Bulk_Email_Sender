ec600e6dd4f8617650af47037b06cb517e3c4659
Ankit Sharma <ankitsharma290101@gmail.com>
2025-01-09 14:40:47 +0530

    dburi

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-exports.dbConnect = async () => {
-  await mongoose
-    .connect("mongodb+srv://Ankit290101:MongoDb@cluster0.kdeds.mongodb.net/", {
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("Db connected success ✅"))
-    .catch((err) => console.error(err));
+      connectTimeoutMS: 10000, // 10 seconds timeout
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit with failure
+  }
 };
+
+module.exports = { dbConnect };
